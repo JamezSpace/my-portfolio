b725a5a68a5e746759666856042a59e92cfe21a7
Jamez <adobeclip2003@gmail.com>
2024-12-31 00:49:31 +0100

    Initial commit

---
+++ src/app/pages/index/index.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+
+@Component({
+    selector: 'index',
+    imports: [NavBarComponent],
+    templateUrl: './index.component.html',
+    styleUrl: './index.component.css'
+})
+export class IndexComponent {
+
+}
